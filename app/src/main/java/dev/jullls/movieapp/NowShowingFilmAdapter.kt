aeb83593353a13bcbb60c336c81ded83d5ed1ba3
Jullliya <vamoyu2@gmail.com>
2024-07-02 22:06:25 +0300

    [feature] now showing and popular adapters use binding

--- app/src/main/java/dev/jullls/movieapp/NowShowingFilmAdapter.kt
+++ app/src/main/java/dev/jullls/movieapp/NowShowingFilmAdapter.kt
@@ -3,22 +3,27 @@ package dev.jullls.movieapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import dev.jullls.movieapp.databinding.ItemNowShowingFilmBinding
 
 class NowShowingFilmAdapter(private val filmList: List<Film>):
     RecyclerView.Adapter<NowShowingFilmAdapter.FilmViewHolder>() {
 
-    class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val filmImageView: ImageView = itemView.findViewById(R.id.film_item_poster_nv)
-        val filmName: TextView = itemView.findViewById(R.id.film_item_name_nv)
-        val filmRating: TextView = itemView.findViewById(R.id.film_item_rating_nv)
+    class FilmViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        private val binding = ItemNowShowingFilmBinding.bind(view)
+
+        fun bind(film: Film){
+            with(binding){
+                ivItemNowShowingPoster.setImageResource(film.image)
+                tvItemNowShowingName.text = film.name
+                tvItemNowShowingRating.text = film.rating
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FilmViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.now_showing_recyclerview_item, parent, false)
+            R.layout.item_now_showing_film, parent, false)
         return FilmViewHolder(view)
     }
 
@@ -28,8 +33,6 @@ class NowShowingFilmAdapter(private val filmList: List<Film>):
 
     override fun onBindViewHolder(holder: FilmViewHolder, position: Int) {
         val film = filmList[position]
-        holder.filmImageView.setImageResource(film.filmImage)
-        holder.filmName.text = film.filmName
-        holder.filmRating.text = film.filmRating
+        holder.bind(film)
     }
 }
--- app/src/main/java/dev/jullls/movieapp/PopularFilmAdapter.kt
+++ app/src/main/java/dev/jullls/movieapp/PopularFilmAdapter.kt
@@ -6,20 +6,27 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import dev.jullls.movieapp.databinding.ItemPopularFilmBinding
 
-class PopularFilmAdapter(private val filmList: List<PopularFilms>):
+class PopularFilmAdapter(private val filmList: List<Film>):
     RecyclerView.Adapter<PopularFilmAdapter.FilmViewHolder>() {
 
-    class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val filmImageView: ImageView = itemView.findViewById(R.id.film_item_poster_p_image)
-        val filmName: TextView = itemView.findViewById(R.id.film_item_name_p)
-        val filmRating: TextView = itemView.findViewById(R.id.film_item_rating_p)
-        val filmDuration: TextView = itemView.findViewById(R.id.film_item_duration_p)
+    class FilmViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        private val binding = ItemPopularFilmBinding.bind(view)
+
+        fun bind(film: Film){
+            with(binding){
+                ivItemPopularPoster.setImageResource(film.image)
+                tvItemPopularName.text = film.name
+                tvItemPopularRating.text = film.rating
+                tvItemPopularDuration.text = film.duration
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FilmViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.popular_recyclerview_item, parent, false)
+            R.layout.item_popular_film, parent, false)
         return FilmViewHolder(view)
     }
 
@@ -29,9 +36,6 @@ class PopularFilmAdapter(private val filmList: List<PopularFilms>):
 
     override fun onBindViewHolder(holder: FilmViewHolder, position: Int) {
         val film = filmList[position]
-        holder.filmImageView.setImageResource(film.filmImage)
-        holder.filmName.text = film.filmName
-        holder.filmRating.text = film.filmRating
-        holder.filmDuration.text = film.filmDuration
+        holder.bind(film)
     }
 }
