2931bc4fb32c035f2c52f12dc04ee14796a7b099
Jullliya <vamoyu2@gmail.com>
2024-07-02 16:15:01 +0300

    [feature] actor adapter use binding

--- app/src/main/java/dev/jullls/movieapp/ActorAdapter.kt
+++ app/src/main/java/dev/jullls/movieapp/ActorAdapter.kt
@@ -3,32 +3,35 @@ package dev.jullls.movieapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import dev.jullls.movieapp.databinding.ItemActorBinding
 
-class ActorAdapter(private val castList: List<Actor>):
-    RecyclerView.Adapter<ActorAdapter.CastViewHolder>() {
+class ActorAdapter(private val actorList: List<Actor>):
+    RecyclerView.Adapter<ActorAdapter.ActorViewHolder>() {
 
-    class CastViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val castImageView: ImageView = itemView.findViewById(R.id.detail_movie_cast_item_image)
-        val castName: TextView = itemView.findViewById(R.id.detail_movie_cast_item_name)
+    class ActorViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        private val binding = ItemActorBinding.bind(view)
+
+        fun bind(actor: Actor){
+            with(binding){
+                ivImage.setImageResource(R.drawable.cast_1)
+                tvName.text = actor.name
+            }
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CastViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActorViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(
             R.layout.detail_movie_layout, parent, false)
-        return CastViewHolder(view)
+        return ActorViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        return castList.size
+        return actorList.size
     }
 
-    override fun onBindViewHolder(holder: CastViewHolder, position: Int) {
-        val cast = castList[position]
-        holder.castImageView.setImageResource(cast.image)
-        holder.castName.text = cast.name
-
+    override fun onBindViewHolder(holder: ActorViewHolder, position: Int) {
+        val actor = actorList[position]
+        holder.bind(actor)
     }
 }
